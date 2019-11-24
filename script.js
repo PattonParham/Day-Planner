54e6c046e1381eba9f8532b396ca3eda6b254b6a
Patton Parham <pattonparham@Pattons-MacBook-Pro.local>
2019-11-24 13:41:17 -0600

    All Saves are working

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var FourPM = $("#Four");
 var FivePM = $("#Five");
 
 
-
+console.log(moment().hours())
 
 //Current Date displayed. Use Date.getTime() to figure this out - scratch that
 //getFullYear(), getMonth(), getDate() are what I want to use
@@ -18,8 +18,29 @@ var FivePM = $("#Five");
 //$("#date").text = (Day.getMonth()) + "/" + (Day.getDate()) + "/" + (Day.getFullYear());
 //document.getElementById("date").innerHTML = (Day.getMonth() + 1) + "/" + (Day.getDate()) + "/" + (Day.getFullYear());
 
-console.log(Day)
+//console.log(Day)
+function styleTextBox() {
 
+var currentHours = moment().hours();
+
+$(".text").each(function(){
+
+var time =  $(this).attr("data-time")
+
+
+//if (time < currentHours){
+//background-color: grey;
+//}
+//if (time = currentHours){
+ //   background-color: red
+//}
+//if (time > currentHours){
+//    background-color: green;
+//}
+
+
+})
+}
 
 //Function inputSubmit so that when the user types a string into the input 
 //field for each time block it is displayed exactly as typed in the 
@@ -31,25 +52,59 @@ console.log(Day)
  //Nine.text(NineAM);
 
 //}
+$(".btn-primary").on("click", function(){
+console.log($(this))
+var key = $(this).attr("id");
+console.log(key)
+var value = $(this).parent().siblings(".text").val()
+console.log(value)
+localStorage.setItem(key, value);
+
+})
  
+
+$("#Ten").val(localStorage.getItem("10am"))
+$("#Nine").val(localStorage.getItem("9am"))
+$("#Eleven").val(localStorage.getItem("11am"))
+$("#Twelve").val(localStorage.getItem("12pm"))
+$("#One").val(localStorage.getItem("1pm"))
+$("#Two").val(localStorage.getItem("2pm"))
+$("#Three").val(localStorage.getItem("3pm"))
+$("#Four").val(localStorage.getItem("4pm"))
+$("#Five").val(localStorage.getItem("5pm"))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //Function Save to "save" the display data in the local storage, so that 
 //when the page is refreshed, the display data (text from input data diplayed)
 //will remain in each block
 
 
 
-var btnNueve = $("#nueve");
-btnNueve.click(function(){
-    var NineAM = $("#Nine");
-jQuery.data(NineAM.html);
+//var btnNueve = $("#nueve");
+//btnNueve.click(function(){
+   // var NineAM = $("#Nine");
+//jQuery.data(NineAM.html);
 //var valueNine;
 //Nine = NineAM.text;
 //switch ((btnNueve).index(this)){
 //case 0:
 //valueNine = $.data(Nine);
-});
+//});
 //NineAM.text(valueNine);
 
 
 //localStorage.setitem("9am","...")
-//$(#9am).val(localStorage(gettem))
+
+
